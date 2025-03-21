b92be28d30f1796c4f47129c508e9f4dd49a5cf7
SantiMAlvarez <smartinezalvarez@mail.austral.edu.ar>
2025-03-21 11:07:44 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,12 @@
 def math():
-    a = 57
-    b = 7
+    A = 57
+    B = 7
+
+    print (A + B)
+    print (A * B)
+    mas = A + B
+    print (mas/2)
+    print (A//B)
+    print (A%B)
+    print (A/B)
+   
